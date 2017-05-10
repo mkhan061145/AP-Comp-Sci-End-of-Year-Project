f6aecda3e210a3295fde6c9eccdc199919aca41f
sahanaiye2893 <sahanaiye2893@k12.ipsd.org>
2017-05-10 08:05:27 -0500

    Update PayrollProject.java

--- PayrollProject.java
+++ PayrollProject.java
@@ -13,7 +13,19 @@ public class PayrollProject {
     
     public static void main(String[] args) {
     	
-    	// TODO, add your application code
-    	System.out.println("Hello World!");
+     JFrame frame = new JFrame("JFrame Example");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+       JLabel label = new JLabel("This is a label!");
+        JButton button = new JButton();
+        button.setText("Press me");
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 }
