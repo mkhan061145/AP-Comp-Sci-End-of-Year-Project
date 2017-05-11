8697f6f891400baf700adaf8772a41a788911253
sahanaiye2893 <sahanaiye2893@k12.ipsd.org>
2017-05-11 07:51:39 -0500

    Update Employed.java

--- Employed.java
+++ Employed.java
@@ -1,3 +1,21 @@
 
 public class Employed {
+  
+  
+  private String first;
+  private String last;
+  private String id;
+  private double pay;
+    
+  public Employed(String firstName, String lastName, String identification,double hourlyPay){
+  first=firstName;
+    last=lastName;
+    id=identification;
+    pay=hourlyPay;
+  }
+  
+  
+  
+  
+  
 }
