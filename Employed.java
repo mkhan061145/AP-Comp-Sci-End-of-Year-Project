fd54d3eaaad5dc9bf0fea3342d9b598aef2dc07b
mkhan061145 <mkhan061145@gmail.com>
2017-05-12 07:50:13 -0500

    Update Employed.java

--- Employed.java
+++ Employed.java
@@ -1,17 +1,18 @@
 
 public class Employed {
-  
-  
   private String first;
   private String last;
   private String id;
   private double pay;
     
-  public Employed(String firstName, String lastName, String identification,double hourlyPay){
-  first=firstName;
-    last=lastName;
-    id=identification;
-    pay=hourlyPay;
+  public Employed(String firstName, String lastName, String identification, double hourlyPay){
+    first = firstName;
+    last = lastName;
+    id = identification;
+    pay = hourlyPay;
+  }
+  public int getWeeklyWage(double hoursWorked){
+    return pay * hoursWorked;
   }
   
   
